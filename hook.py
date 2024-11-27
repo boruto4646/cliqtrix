7d8b912ed00a078256c5b362506701a7b21b569c
SUMAN <153175938+boruto4646@users.noreply.github.com>
2024-11-27 22:58:08 +0530

    Update hook.py

--- hook.py
+++ hook.py
@@ -40,4 +40,7 @@ def zobot_webhook():
     return jsonify({"response": wiki_summary})
 
 if __name__ == "__main__":
-    app.run(port=5000)
+    import os
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
+
